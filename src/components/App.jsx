bc413a31078dfbcd98137560daa06faac51e63e2
lllastun <lllastun@gmail.com>
2023-05-16 14:50:29 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Main } from './main/Main';
 import { SideBar } from './sidebar/SideBar';
 import style from './App.module.css';
 
+
 export const App = () => {
   return (
     <div className={style.body}>
